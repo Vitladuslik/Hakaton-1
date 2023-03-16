2ba8ee2dec7d9ef459da879a5b1417e3cf2a12fe
nyuto <nuytonyka@gmail.com>
2023-03-16 20:16:06 +0200

    add schema liquibase

--- src/main/java/com/template/entities/Restaurant.java
+++ src/main/java/com/template/entities/Restaurant.java
@@ -1,11 +1,24 @@
 package com.template.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Accessors(chain = true)
+@Table(name = "restaurants")
 public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
     String city;
     String name;
     Integer estimatedCost;
     String averageRating;
     Integer votes;
-    Integer id;
-
 }
